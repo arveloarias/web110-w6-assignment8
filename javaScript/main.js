4f2b9f7447139a84aecda0c69c9a1fe23e3bb1b9
Juan Arvelo <arveloarias@gmail.com>
2022-07-29 13:32:26 -0400

    Super commit of project 8, web 110

---
+++ javaScript/main.js
@@ -0,0 +1,9 @@
+donation = PhotoGhp.querySelector("#donation").value
+
+
+PhotoGhp.querySelector("#btnDonate").addEventListener("click", ()=>{
+        PhotoGhp.querySelector(".donation").style = "display:none";
+        PhotoGhp.querySelector(".thanksMsg").style = "display: block";
+})    
+
+    
